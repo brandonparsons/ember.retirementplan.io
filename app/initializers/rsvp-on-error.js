662c8b70ce6450bc4432964915ed4e603a166a1b
Brandon Parsons <parsons.brandon@gmail.com>
2014-06-05 05:51:30 -0600

    Catch error related to unconfirmed email addresses.

--- app/initializers/rsvp-on-error.js
+++ app/initializers/rsvp-on-error.js
@@ -4,7 +4,9 @@ export default {
   initialize: function(container, application) {
 
     Ember.RSVP.on('error', function (error) {
-      var errorMessage, session, sticky;
+      var errorMessage, sticky;
+      var session = application.__container__.lookup('controller:application').get('session');
+      var router  = application.__container__.lookup('router:main');
 
       if (window.ENV.debug) {
         Ember.warn("Caught Ember.RSVP Error!");
@@ -22,30 +24,40 @@ export default {
       //   message: errorMessage
       // });
 
-      // If it was a 401 error, clear the session and 'reboot'. Otherwise, determine
-      // the error message and display it.
-      if (error.errorThrown === 'Unauthorized' || error.jqXHR.status === 401) {
+      var _determineErrorMessage = function(error) {
+        if (error && error.jqXHR && error.jqXHR.responseJSON && error.jqXHR.responseJSON.message) {
+          return error.jqXHR.responseJSON.message;
+        } else if (error && error.message) {
+          return error.message;
+        } else if (error && error.jqXHR && error.jqXHR.responseJSON && error.jqXHR.responseJSON.error) {
+          return error && error.jqXHR && error.jqXHR.responseJSON && error.jqXHR.responseJSON.error;
+        } else {
+          return 'Sorry - something went wrong.';
+        }
+      };
 
-        session = application.__container__.lookup('controller:application').get('session');
+
+      if (error.errorThrown === 'Unauthorized' || error.jqXHR.status === 401) {
+        // If it was a 401 error, clear the session and 'reboot'
         session.clear();
         window.location.replace(window.ENV.baseURL);
 
-      } else {
+      } else if (error.errorThrown === 'Forbidden' || error.jqXHR.status === 403) {
+        // If it is a 403, display the message and redirect to an appropriate page
+        errorMessage  = _determineErrorMessage(error);
 
-        if (error && error.jqXHR && error.jqXHR.responseJSON && error.jqXHR.responseJSON.message) {
-          errorMessage = error.jqXHR.responseJSON.message;
-        } else if (error && error.message) {
-          errorMessage = error.message;
-        } else if (error && error.jqXHR && error.jqXHR.responseJSON && error.jqXHR.responseJSON.error) {
-          errorMessage = error && error.jqXHR && error.jqXHR.responseJSON && error.jqXHR.responseJSON.error;
+        if (error.jqXHR.responseJSON.reason === 'email_confirmation') {
+          session.clear();
+          router.transitionTo('email_confirmation');
         } else {
-          errorMessage = 'Sorry - something went wrong.';
+          //
         }
-
-        sticky = error && error.jqXHR && error.jqXHR.responseJSON && error.jqXHR.responseJSON.sticky || false;
-
         application.setFlash('error', errorMessage, sticky);
-
+      } else {
+        // Not any of the errors above. Just grab the error message and process it.
+        errorMessage  = _determineErrorMessage(error);
+        sticky        = error && error.jqXHR && error.jqXHR.responseJSON && error.jqXHR.responseJSON.sticky || false;
+        application.setFlash('error', errorMessage, sticky);
       }
 
     }); // RSVP.onError
