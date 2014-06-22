28f2aca177e1da6cfca29b532c485bc3e41112bf
Brandon Parsons <parsons.brandon@gmail.com>
2014-06-22 07:21:59 -0600

    Adjust fontawesome-icon component to have size & spin capabilities. Use everywhere instead of <i class='....

--- app/components/fontawesome-icon.js
+++ app/components/fontawesome-icon.js
@@ -1,16 +1,39 @@
 import Ember from 'ember';
 
 var FontawesomeIconComponent = Ember.Component.extend({
-  type: null, // expected ta be bound
+  type: null, // Bound
+  size: null, // Bound
+  spin: false,
 
   withForcedSpace: true,
 
   tagName: 'i',
-  classNameBindings: [':fa', 'faType'],
+  classNameBindings: [':fa', 'faType', 'faSize', 'faSpin'],
 
   faType: function() {
     return 'fa-' + this.get('type');
-  }.property('type')
+  }.property('type'),
+
+  faSize: function() {
+    var size = this.get('size');
+    if ( size === '1x' || Ember.isEmpty(size) ) {
+      return null;
+    } else if (size === '2x') {
+      return 'fa-2x';
+    } else if (size === '3x') {
+      return 'fa-3x';
+    } else {
+      return null;
+    }
+  }.property('size'),
+
+  faSpin: function() {
+    if (this.get('spin')) {
+      return 'fa-spin';
+    } else {
+      return null;
+    }
+  }.property('spin'),
 
 });
 
