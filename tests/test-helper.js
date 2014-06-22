37857821f16f05a62f5c8acb535b8164f07433b0
Brandon Parsons <parsons.brandon@gmail.com>
2014-06-22 08:34:16 -0600

    Add test helpers from old project.

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -4,3 +4,48 @@ import { setResolver } from 'ember-qunit';
 setResolver(resolver);
 
 document.write('<div id="ember-testing-container"><div id="ember-testing"></div></div>');
+
+function exists(selector) {
+  return !!find(selector).length;
+}
+
+function textMatches(selector, text) {
+  var message = "The text matches expectations on " + selector;
+  equal(find(selector).text(), text, message);
+}
+
+function correctNumberItems(selector, length) {
+  var message = "The number of items matches expectations on " + selector;
+  equal(find(selector).length, length, message);
+}
+
+var testing = function(app) {
+  var helper = {
+    container: function() {
+      return app.__container__;
+    },
+    store: function() {
+      return helper.container().lookup("store:main");
+    },
+    controller: function(name) {
+      return helper.container().lookup("controller:" + name);
+    },
+    path: function() {
+      return helper.controller('application').get('currentPath');
+    }
+  };
+
+  return helper;
+};
+
+Ember.Test.registerHelper('path', function(app) {
+  return testing(app).path();
+});
+
+Ember.Test.registerHelper('store', function(app) {
+  return testing(app).store();
+});
+
+Ember.Test.registerHelper('currentUser', function(app) {
+  return testing(app).controller('user.current');
+});
