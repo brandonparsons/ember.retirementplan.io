e1999c4d1d4fb8c954037f76d6a2dd0786524cfa
Brandon Parsons <parsons.brandon@gmail.com>
2014-07-16 01:18:02 -0600

    Pull all route logic out of controller

--- app/controllers/user/dashboard.js
+++ app/controllers/user/dashboard.js
@@ -109,37 +109,4 @@ export default Ember.Controller.extend({
   trackPortfolioActive:   Ember.computed.equal('onStep', 'TRACKED_PORTFOLIO'),
   trackPortfolioEnabled:  Ember.computed.and('questionnaireComplete', 'portfolioSelectionComplete', 'simulationComplete'),
 
-
-  /////////////
-  // Actions //
-  /////////////
-
-  actions: {
-
-    actionBasedTransitionTo: function(route) {
-      // Check if it should be enabled at this point (user progress) prior to
-      // transitioning.
-      var doTransition;
-
-      if (route === 'questionnaire') {
-        doTransition = this.get('questionnaireEnabled');
-      } else if (route === 'select_portfolio') {
-        doTransition = this.get('portfolioSelectionEnabled');
-      } else if ( route.match(/retirement_simulation\..*/i) ) {
-        doTransition = this.get('retirementSimulationEnabled');
-      } else if ( route.match(/tracked_portfolio\..*/i) ) {
-        doTransition = this.get('trackPortfolioEnabled');
-      } else {
-        throw new Error('Invalid route name'); // Shouldn't get here unless you add additional buttons
-      }
-
-      if (doTransition) {
-        return true; // Let this bubble up to the route. It was causing odd transition errors from the controller.
-      } else {
-        return false;
-      }
-    }
-
-  }
-
 });
--- app/routes/user/dashboard.js
+++ app/routes/user/dashboard.js
@@ -4,7 +4,25 @@ export default Ember.Route.extend({
 
   actions: {
     actionBasedTransitionTo: function(route) {
-      this.transitionTo(route);
+      // Check if it should be enabled at this point (user progress) prior to
+      // transitioning.
+      var doTransition;
+
+      if (route === 'questionnaire') {
+        doTransition = this.get('controller.questionnaireEnabled');
+      } else if (route === 'select_portfolio') {
+        doTransition = this.get('controller.portfolioSelectionEnabled');
+      } else if ( route.match(/retirement_simulation\..*/i) ) {
+        doTransition = this.get('controller.retirementSimulationEnabled');
+      } else if ( route.match(/tracked_portfolio\..*/i) ) {
+        doTransition = this.get('controller.trackPortfolioEnabled');
+      } else {
+        throw new Error('Invalid route name'); // Shouldn't get here unless you add additional buttons
+      }
+
+      if (doTransition) {
+        this.transitionTo(route);
+      }
     }
   }
 
