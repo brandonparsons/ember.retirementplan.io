56d35bdf697753c4cd537e63dcded15b44417b42
Brandon Parsons <parsons.brandon@gmail.com>
2014-08-03 09:31:54 -0600

    Try removing fastclick to see if it helps graphs on mobile

--- app/views/application.js
+++ app/views/application.js
@@ -5,10 +5,21 @@ import Ember from 'ember';
 export default Ember.View.extend({
 
   didInsertElement: function() {
-    var header, headroom;
 
-    FastClick.attach(document.body);
+    // HeadRoom.js
+    var header = $('.navbar')[0];
+    var headroom = new Headroom(header);
+    headroom.init();
+    $('body').tooltip({
+      selector: '[data-toggle=tooltip]',
+      html: true
+    });
 
+    // FastClick.js
+    // Removing for now, see if it helps graphs work on mobile....
+    // FastClick.attach(document.body);
+
+    // Navbar hover fix
     $('.dropdown-toggle').click(function() {
       var location;
       location = $(this).attr('href');
@@ -20,14 +31,7 @@ export default Ember.View.extend({
       }
     });
 
-    header = $('.navbar')[0];
-    headroom = new Headroom(header);
-    headroom.init();
-    $('body').tooltip({
-      selector: '[data-toggle=tooltip]',
-      html: true
-    });
-
+    // Bootstrap.js popovers
     $('body').popover({
       selector: '[data-toggle=popover]',
       html: true
