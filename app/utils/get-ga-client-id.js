b13f750d0d1f0a79d7cdfd3eb535e1ed7bb76d17
Brandon Parsons <parsons.brandon@gmail.com>
2014-08-12 06:36:59 -0600

    Firefox doesnt like ga's built-in get tracker method. Fall back to extracting from the cookie

--- app/utils/get-ga-client-id.js
+++ app/utils/get-ga-client-id.js
@@ -1,3 +1,20 @@
+var getCookie = function (name) {
+  var value = "; " + document.cookie;
+  var parts = value.split("; " + name + "=");
+  if (parts.length == 2) return parts.pop().split(";").shift();
+};
+
+var extractGAClientIdFromCookie = function() {
+  var gaCookieValue = getCookie("_ga");
+  var clientIdRegex = /^GA\d\.\d\.(.+)$/;
+  var matchData     = gaCookieValue.match(clientIdRegex);
+  if (matchData) {
+    return matchData[1];
+  } else {
+    return null;
+  }
+};
+
 export default function() {
   var clientId;
   if (!window.ga) { return null; }
@@ -6,5 +23,9 @@ export default function() {
     clientId = tracker.get('clientId');
   });
 
-  return clientId;
+  if (clientId) {
+    return clientId;
+  } else {
+    return extractGAClientIdFromCookie();
+  }
 }
