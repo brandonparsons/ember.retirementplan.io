a8dcb4403116b9f6b8b3b5f0e70da4e042759b44
Brandon Parsons <parsons.brandon@gmail.com>
2014-08-11 22:09:41 -0600

    Sidebar menu toggle.
    - Provided ability to toggle sidebar menu from navbar, and close via button in the menu
    - Changed bootstrap breakpoint for nav collapse to something larger, as we added another button in the navbar
    - Pushed the menu on mobile to be below the main content

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -3,5 +3,15 @@ import Ember from 'ember';
 export default Ember.ObjectController.extend({
   needs: ['user/current'],
   currentUser: Ember.computed.alias('controllers.user/current'),
-  showSidebar: Ember.computed.alias('currentUser.hasTrackedPortfolio')
+
+  hasOpenedSidebarMenu: true, // Default to open
+  shouldShowSidebar:    Ember.computed.alias('currentUser.hasTrackedPortfolio'),
+  showSidebar:          Ember.computed.and('shouldShowSidebar', 'hasOpenedSidebarMenu'),
+
+  actions: {
+    toggleSidebar: function() {
+      this.toggleProperty('hasOpenedSidebarMenu');
+    }
+  }
+
 });
