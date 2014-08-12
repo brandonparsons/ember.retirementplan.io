4212ad22650e22de8ef6c18940aa50dd9b0460ab
Brandon Parsons <parsons.brandon@gmail.com>
2014-08-12 06:38:44 -0600

    jsHint fixes

--- app/utils/get-ga-client-id.js
+++ app/utils/get-ga-client-id.js
@@ -1,7 +1,9 @@
 var getCookie = function (name) {
   var value = "; " + document.cookie;
   var parts = value.split("; " + name + "=");
-  if (parts.length == 2) return parts.pop().split(";").shift();
+  if (parts.length === 2) {
+    return parts.pop().split(";").shift();
+  }
 };
 
 var extractGAClientIdFromCookie = function() {
