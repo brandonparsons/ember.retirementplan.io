c1479e446ac3ff0afa0e660a84354e89983b2b11
Brandon Parsons <parsons.brandon@gmail.com>
2014-08-14 06:28:09 -0600

    Force a check for user's email confirmation status before visiting dashboard

--- app/routes/questionnaire.js
+++ app/routes/questionnaire.js
@@ -4,6 +4,9 @@ export default Ember.Route.extend(
   Ember.SimpleAuth.AuthenticatedRouteMixin, {
 
   afterModel: function() {
+    // Because the user will need to accept the T&C's right off the bat, let's
+    // check before they get into the questionnaire. This will save them from
+    // filling out the form, only to be sent to accept_terms
     var terms = this.controllerFor('user.current').get('acceptedTerms');
     if (!terms) {
       this.transitionTo('accept_terms');
---
+++ app/routes/user/dashboard.js
@@ -0,0 +1,18 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  beforeModel: function() {
+    // Check that the user does not need to confirm their email before we
+    // proceed any further. (This is the default route after a login).
+    var confirmed = this.controllerFor('user.current').get('confirmed');
+    if (!confirmed) {
+      // Clear the session (log the user out) similar to the error handler. We
+      // aren't technically checking for confirmation *BEFORE* the user visits
+      // most routes, so save them heartache and force the issue.
+      this.controllerFor('application').get('session').clear();
+
+      RetirementPlan.setFlash('notice', "You must confirm your email before proceeding.");
+      this.transitionTo('email_confirmation');
+    }
+  }
+})
