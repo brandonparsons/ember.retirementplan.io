e122d71942c25dabc857a26c00c93ab7da3c361a
Brandon Parsons <parsons.brandon@gmail.com>
2014-09-01 16:19:47 -0600

    Make use of btn-primary and btn-success more consistent

--- app/components/form-submit-button.js
+++ app/components/form-submit-button.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Forms.FormSubmitComponent.extend({
   classNames: ['form-group', 'inline', 'float-left'], // On the parent form-group
-  classes:    'btn btn-primary', // On the button itself
+  classes:    'btn btn-success', // On the button itself
   horiClass:  'space-left' // Inner div
 });
