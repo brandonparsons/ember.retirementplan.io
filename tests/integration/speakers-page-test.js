d2e335f82f6eee30608b095c7a73e636c65ff286
Brandon Parsons <parsons.brandon@gmail.com>
2014-08-31 22:42:11 -0600

    Save a helper function for pretender, to grab post body data

--- tests/integration/speakers-page-test.js
+++ tests/integration/speakers-page-test.js
@@ -6,6 +6,22 @@
 
 // var App, server;
 
+// // Helper function from https://github.com/eviltrout/login/commit/96ea107cc79ddb27d649aa9c8b8990bbd0ce04b6
+// function parsePostData(query) {
+//   var result = {};
+//   query.split("&").forEach(function(part) {
+//     var item = part.split("=");
+//     result[item[0]] = decodeURIComponent(item[1]);
+//   });
+//   return result;
+// }
+// // Usage
+// this.post("/session", function(request) {
+//   var data = parsePostData(request.requestBody);
+//   if (data.password === "secret") {
+//     ......
+
+
 // module('Integration - Speaker Page', {
 //   setup: function() {
 //     App = startApp();
